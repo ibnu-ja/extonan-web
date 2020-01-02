acca849edfc1c0d7c7daad4ed1271a9e74a3778a
ibnu-ja <ibnu.magang@gmail.com>
2020-01-02 20:46:31 +0700

    jadwal database

---
+++ database/migrations/2020_01_02_123345_create_jadwals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJadwalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jadwals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->tinyInteger('hari');
+            $table->time('jam');
+            $table->string('anime_id');
+            $table->foreign('anime_id')->references('id')->on('animes')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jadwals');
+    }
+}
