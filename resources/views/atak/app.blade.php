3e87e366315d45e407d90942e46aeb1b0ef670bb
ibnu-ja <ibnu.magang@gmail.com>
2020-01-18 15:15:37 +0700

    [Frontend] Footer

--- resources/views/atak/app.blade.php
+++ resources/views/atak/app.blade.php
@@ -35,84 +35,21 @@
   <footer class="page-footer stylish-color-dark mt-4 pt-4">
     <!-- Footer Links -->
     <div class="container-fluid">
-      <!-- Footer links -->
-      <div class="row text-center text-md-left mt-3 pb-3">
-        <!-- Grid column -->
-        <div class="col-md-3 col-lg-3 col-xl-3 mr-auto mt-3">
-          <h6 class="text-uppercase mb-4 font-weight-bold">Company name</h6>
-          <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit amet,
-            consectetur
-            adipisicing elit.</p>
-        </div>
-        <!-- Grid column -->
-        <hr class="w-100 clearfix d-md-none">
-        <!-- Grid column -->
-        <div class="col-md-2 col-lg-2 col-xl-2 mr-auto mt-3">
-          <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
-          <p>
-            <a href="#!">MDBootstrap</a>
-          </p>
-          <p>
-            <a href="#!">MDWordPress</a>
-          </p>
-          <p>
-            <a href="#!">BrandFlow</a>
-          </p>
-          <p>
-            <a href="#!">Bootstrap Angular</a>
-          </p>
-        </div>
-        <!-- Grid column -->
-        <hr class="w-100 clearfix d-md-none">
-        <!-- Grid column -->
-        <div class="col-md-3 col-lg-2 col-xl-2 mr-auto mt-3">
-          <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
-          <p>
-            <a href="#!">Your Account</a>
-          </p>
-          <p>
-            <a href="#!">Become an Affiliate</a>
-          </p>
-          <p>
-            <a href="#!">Shipping Rates</a>
-          </p>
-          <p>
-            <a href="#!">Help</a>
-          </p>
-        </div>
-        <!-- Third column -->
-        <hr class="w-100 clearfix d-md-none">
-        <!-- Grid column -->
-        <div class="col-md-4 col-lg-3 col-xl-3 mr-auto mt-3">
-          <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
-          <p>
-            <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-          <p>
-            <i class="fas fa-envelope mr-3"></i> info@example.com</p>
-          <p>
-            <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-          <p>
-            <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-        </div>
-        <!-- Grid column -->
-      </div>
-      <!-- Footer links -->
-      <hr>
       <div class="row py-3 d-flex align-items-center">
         <!-- Grid column -->
         <div class="col-md-7 col-lg-8">
           <!-- Copyright -->
           <p class="text-center text-md-left grey-text">
-            © 2019 Copyright: <a href="https://mdbootstrap.com/education/bootstrap/" target="_blank"> MDBootstrap.com
+            © 2019 Copyright: Extonan
             </a>
           </p>
           <!-- Copyright -->
         </div>
         <!-- Grid column -->
         <!-- Grid column -->
-        <div class="col-md-5 col-lg-4 ml-lg-0">
+        <div class="col ml-lg-0">
           <!-- Social buttons -->
-          <div class="social-section text-center mr-auto text-md-left">
+          <div class="social-section text-center mr-auto text-right">
             <ul class="list-unstyled list-inline">
               <li class="list-inline-item">
                 <a class="btn-floating btn-sm rgba-white-slight">
@@ -122,16 +59,6 @@
               <li class="list-inline-item">
                 <a class="btn-floating btn-sm rgba-white-slight">
                   <i class="fab fa-twitter"></i>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a class="btn-floating btn-sm rgba-white-slight">
-                  <i class="fab fa-google-plus-g"></i>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a class="btn-floating btn-sm rgba-white-slight">
-                  <i class="fab fa-linkedin-in"></i>
                 </a>
               </li>
             </ul>
