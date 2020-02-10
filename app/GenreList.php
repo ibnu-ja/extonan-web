52af620ab6ce6ff578f325401fb16c12de729a23
ibnu-ja <ibnu.magang@gmail.com>
2020-02-11 02:55:24 +0700

    Modifikasi model GenreList set primary key dan incrementing

--- app/GenreList.php
+++ app/GenreList.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class GenreList extends Model
 {
+    protected $primaryKey = 'genre';
+    public $incrementing = false;
+
     public function getRouteKeyName()
     {
         return 'genre';
