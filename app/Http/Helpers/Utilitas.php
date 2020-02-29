e219ede5443ee32f4a5c9e93e3c9688be2bd116c
ibnu-ja <ibnu.magang@gmail.com>
2020-02-29 18:17:19 +0700

    Explode fungsi upload

--- app/Http/Helpers/Utilitas.php
+++ app/Http/Helpers/Utilitas.php
@@ -27,38 +27,39 @@ if (!function_exists('unggah')) {
             $canvas->save($simpan);
         }
     }
-    
 }
-if (!function_exists('unggahResize')) {
-    function unggahResize($file, $tempat, $dimensi = null, $namaFile = null, $hapus = false)
+
+if (!function_exists('unggahResizeNama')) {
+    function unggahResizeNama($file, $tempat, $dimensi, $namaFile)
     {
         $lokasi = storage_path($tempat);
-        if ($hapus)
-            File::deleteDirectory($lokasi);
         if (!File::isDirectory($lokasi)) {
             File::makeDirectory($lokasi);
         }
 
-        if (!empty($namaFile)) {
-            $simpan = $lokasi . '/' . $namaFile . '.' . $file->getClientOriginalExtension();
-            hapus($simpan);
-            Image::make($file)->save($simpan);
-        } else {
-            $simpan = $lokasi . '/' . 'img.' . $file->getClientOriginalExtension();
-            hapus($simpan);
-            Image::make($file)->save($simpan);
-        }
-
-        if (!empty($dimensi)) {
-            $simpan = $lokasi . '/' . $dimensi[0] . '.' . $file->getClientOriginalExtension();
-            hapus($simpan);
-            Image::make($file)->resize($dimensi[0], $dimensi[1],function ($constraint) {
-                $constraint->aspectRatio();
-            })
+        $simpan = $lokasi . '/' . $namaFile . '.' . $file->getClientOriginalExtension();
+        hapus($simpan);
+        Image::make($file)->resize($dimensi[0], $dimensi[1], function ($constraint) {
+            $constraint->aspectRatio();
+        })
             ->resizeCanvas($dimensi[0], $dimensi[1])
             ->save($simpan);
-            
+    }
+}
+if (!function_exists('unggahResize')) {
+    function unggahResize($file, $tempat, $dimensi)
+    {
+        $lokasi = storage_path($tempat);
+        if (!File::isDirectory($lokasi)) {
+            File::makeDirectory($lokasi);
         }
+        $simpan = $lokasi . '/' . $dimensi[0] . '.' . $file->getClientOriginalExtension();
+        hapus($simpan);
+        Image::make($file)->resize($dimensi[0], $dimensi[1], function ($constraint) {
+            $constraint->aspectRatio();
+        })
+            ->resizeCanvas($dimensi[0], $dimensi[1])
+            ->save($simpan);
     }
 }
 if (!function_exists('hapus')) {
